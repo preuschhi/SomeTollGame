8ae5c850c6df1a6f431f8f9496eaa3b55f4b7c04
preuschhi <bmx.lego@gmail.com>
2021-02-04 20:26:54 +0100

    Rotating Shooting Pipe

--- Project/SomeTollGame/Assets/Movement.cs
+++ Project/SomeTollGame/Assets/Movement.cs
@@ -6,21 +6,41 @@ public class Movement : MonoBehaviour
 {
     
     public Rigidbody2D rbPlayer;
+    public Transform playerGunJoint;
 
     void Start()
     {
-       rbPlayer = GetComponent<Rigidbody2D>(); 
+       rbPlayer = GetComponentInChildren<Rigidbody2D>(); 
+       if (playerGunJoint == null) {
+       playerGunJoint = GetComponentInChildren<Transform> ();
+       } 
     }
 
+
     public float moveSpeed = 10f;
+    public Vector2 mousePosition;
+    public float mouseAngle;
+    public float roationSpeed = 10f;
+
+    public Camera cam;
     void FixedUpdate()
     {
 
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
-        rbPlayer.velocity = new Vector2(x * moveSpeed, y * moveSpeed );
+        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+        rbPlayer.velocity = new Vector2(x * moveSpeed, y * moveSpeed);
+
+
+        
+        Vector2 screenPoint = Camera.main.WorldToScreenPoint(playerGunJoint.localPosition);
+        Vector2 offset = new Vector2(mousePosition.x - screenPoint.x, mousePosition.y - screenPoint.y);
+        mouseAngle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
     
-        float xMouse = Input.GetAxis("")
+         playerGunJoint.rotation = Quaternion.Euler(0, 0, mouseAngle);
+
+
     }
-}
+} 
